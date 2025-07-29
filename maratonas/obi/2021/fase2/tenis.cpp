31fdf77fff5d533ff706fa855cc5647c219bae85
israelvaz121 <israelvaz995@gmail.com>
2025-07-29 11:01:37 -0300

    Fase2 2021

---
+++ maratonas/obi/2021/fase2/tenis.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    vector<int> v(4);
+    
+    for (int i = 0; i < 4; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    int ans = abs((v[0] + v[3]) - (v[1] + v[2]));
+
+    cout << ans;
+}
